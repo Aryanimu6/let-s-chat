03ea72cdadb17fa24dac611370f18286a52603b0
Aryanimu6 <aryanvermaimu6@gmail.com>
2024-10-01 14:51:47 +0530

    Update connectMongodb.js

--- backend/db/connectMongodb.js
+++ backend/db/connectMongodb.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
- 
 
-const connectMongodb= async () =>{
-    try{
-        const conn= await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Mongodb connected:${conn.connection.host}`);
-    }
-    catch(console)
-    {
-        console.error(`error connection mongodb:${error.message}`);
-        process.exit(1);
-    }
-}
-export default connectMongodb;
+const connectMongoDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error connection to mongoDB: ${error.message}`);
+		process.exit(1);
+	}
+};
+
+export default connectMongoDB;
